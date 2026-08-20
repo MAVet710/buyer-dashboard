bdc0ac6cb8b1177504c9dfcbf2f576a2c1af650a
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-08-20 13:29:23 -0400

    Install transaction safe migration matcher

--- modules/migration_center/__init__.py
+++ modules/migration_center/__init__.py
@@ -2,6 +2,9 @@
 
 from .models import MigrationBatch, MigrationRecord, MigrationSalesHistory
 from .service import MigrationCenterService, detect_source_system, normalize_import_row
+from .transaction_safe import install_transaction_safe_match
+
+install_transaction_safe_match(MigrationCenterService)
 
 __all__ = [
     "MigrationBatch",
