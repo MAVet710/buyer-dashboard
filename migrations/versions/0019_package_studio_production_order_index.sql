090fbaf72a04d6f08f238c72033def6f46eec3cd
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-08-20 10:12:39 -0400

    Align standalone migration 0019 revision ID

--- migrations/versions/0019_package_studio_production_order_index.sql
+++ migrations/versions/0019_package_studio_production_order_index.sql
@@ -4,7 +4,7 @@ create index if not exists ix_package_studio_runs_production_order_id
     on public.package_studio_runs(production_order_id);
 
 update public.alembic_version
-set version_num = '0019_package_studio_production_order_index'
+set version_num = '0019_pkgstudio_po_index'
 where version_num = '0018_traceability_transactions';
 
 commit;
