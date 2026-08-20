ff266ff24478ace99b27535c11de54e73d341d92
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-08-20 09:53:59 -0400

    Expose Backoffice traceability repository

--- modules/traceability/__init__.py
+++ modules/traceability/__init__.py
@@ -1,10 +1,17 @@
 """Durable traceability transaction and reconciliation foundation."""
 
-from .models import TraceabilityTransaction, TraceabilityTransactionAttempt
+from .backoffice import TraceabilityBackofficeRepository
+from .models import (
+    TraceabilityStatusEvent,
+    TraceabilityTransaction,
+    TraceabilityTransactionAttempt,
+)
 from .repository import TraceabilityRepository, VALID_TRANSITIONS
 
 __all__ = [
+    "TraceabilityBackofficeRepository",
     "TraceabilityRepository",
+    "TraceabilityStatusEvent",
     "TraceabilityTransaction",
     "TraceabilityTransactionAttempt",
     "VALID_TRANSITIONS",
