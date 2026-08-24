665c1d154aec2cf510d3383bc0dc37ee05216570
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-08-23 21:49:52 -0400

    Remove unused brand component

--- frontend/src/components/BrandLogo.tsx
+++
@@ -1,5 +0,0 @@
-import { BRAND_IMAGE_URL } from "../lib/brand";
-
-export function BrandLogo({ className = "brand-logo-image", alt = "DoobieLogic" }: { className?: string; alt?: string }) {
-  return <img className={className} src={BRAND_IMAGE_URL} alt={alt} />;
-}
