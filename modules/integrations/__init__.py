905a590e4ed2d88f229389cec9f785048474991e
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-08-25 16:26:16 -0400

    Export sandbox integration runtime

--- modules/integrations/__init__.py
+++ modules/integrations/__init__.py
@@ -1,4 +1,17 @@
-from .models import IntegrationConfiguration
+from .models import (
+    IntegrationConfiguration,
+    IntegrationSyncAttempt,
+    IntegrationSyncRecord,
+    IntegrationSyncState,
+)
 from .service import IntegrationConfigurationService
+from .sandbox_runtime import SandboxIntegrationRuntime
 
-__all__ = ["IntegrationConfiguration", "IntegrationConfigurationService"]
+__all__ = [
+    "IntegrationConfiguration",
+    "IntegrationConfigurationService",
+    "IntegrationSyncAttempt",
+    "IntegrationSyncRecord",
+    "IntegrationSyncState",
+    "SandboxIntegrationRuntime",
+]
