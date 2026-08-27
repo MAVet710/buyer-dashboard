bad0971fbd3efe631dfc3472a39b0146b061c008
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-08-27 18:22:21 -0400

    Keep availability package import-safe

--- modules/inventory_availability/__init__.py
+++ modules/inventory_availability/__init__.py
@@ -1,8 +1,5 @@
 """Cross-workspace inventory availability and commitment projection."""
 
 from .service import InventoryAvailabilityService
-from .coman_guard import install_coman_availability_guard
-
-install_coman_availability_guard()
 
 __all__ = ["InventoryAvailabilityService"]
