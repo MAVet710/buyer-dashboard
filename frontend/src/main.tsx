03bb70d065c4868104ea8aa3a90276d58ee2f386
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-08-26 20:57:24 -0400

    Mount authenticated app under browser router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthGate } from "./components/AuthGate";
 import { AppSupportButton, MarketingContactChannels } from "./components/ContactChannels";
@@ -37,10 +38,14 @@ createRoot(document.getElementById("root")!).render(
       {portalToken ? <CommercePortalPage token={portalToken} /> : marketing ? <>
         {betaPage ? <BetaPartnerPage /> : <MarketingHome />}
         <MarketingContactChannels />
-      </> : <AuthGate><>
-        <App />
-        <AppSupportButton />
-      </></AuthGate>}
+      </> : <AuthGate>
+        <BrowserRouter>
+          <>
+            <App />
+            <AppSupportButton />
+          </>
+        </BrowserRouter>
+      </AuthGate>}
     </QueryClientProvider>
   </StrictMode>,
 );
