5a3c7adf841aad79f1cf68172e98a53795b3c51c
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-08-29 10:23:45 -0400

    Point shared DoobieLogic branding to new master logo

--- frontend/src/lib/brand.ts
+++ frontend/src/lib/brand.ts
@@ -1,4 +1,4 @@
-export const BRAND_IMAGE_URL = "https://raw.githubusercontent.com/MAVet710/buyer-dashboard/main/IMG_7158.PNG";
+export const BRAND_IMAGE_URL = "/doobielogic-logo.webp";
 export const APP_URL = "https://ops.doobielogic.io/";
 export const SUPPORT_EMAIL = "support@doobielogic.io";
 export const HELP_EMAIL = "help@doobielogic.io";
