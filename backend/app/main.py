43b8d719753e22d55f3866779d633553653285a9
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-08-30 19:45:22 -0400

    Register QuickBooks purchasing integration router

--- backend/app/main.py
+++ backend/app/main.py
@@ -53,6 +53,7 @@ from .routers.admin_user_create import router as admin_user_create_router
 from .routers.admin_uploads import router as admin_uploads_router
 from .routers.integrations import router as integrations_router
 from .routers.native_integrations import router as native_integrations_router
+from .routers.quickbooks_purchasing import router as quickbooks_purchasing_router
 from .routers.sandbox_integrations import router as sandbox_integrations_router
 from .routers.label_printing import router as label_printing_router
 from .routers.printing_external import router as printing_external_router
@@ -278,6 +279,7 @@ app.add_api_route(
 app.include_router(admin_uploads_router, prefix=settings.api_prefix)
 app.include_router(integrations_router, prefix=settings.api_prefix)
 app.include_router(native_integrations_router, prefix=settings.api_prefix)
+app.include_router(quickbooks_purchasing_router, prefix=settings.api_prefix)
 app.include_router(sandbox_integrations_router, prefix=settings.api_prefix)
 app.include_router(label_printing_router, prefix=settings.api_prefix)
 app.include_router(webhooks_router, prefix=settings.api_prefix)
