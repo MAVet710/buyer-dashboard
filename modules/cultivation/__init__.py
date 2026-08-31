9ac69025005520a1e379e4611fe87c68990f4019
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-08-31 14:50:11 -0400

    Register cultivation batch genealogy models

--- modules/cultivation/__init__.py
+++ modules/cultivation/__init__.py
@@ -1,6 +1,16 @@
 """Durable cultivation inventory."""
 
+from .batch_models import CultivationPlantGroup, CultivationPlantGroupMember, CultivationPlantParentLink
 from .models import CultivationPlant, CultivationPlantEvent
 from .service import CultivationService
+from .batches import CultivationBatchService
 
-__all__ = ["CultivationPlant", "CultivationPlantEvent", "CultivationService"]
+__all__ = [
+    "CultivationBatchService",
+    "CultivationPlant",
+    "CultivationPlantEvent",
+    "CultivationPlantGroup",
+    "CultivationPlantGroupMember",
+    "CultivationPlantParentLink",
+    "CultivationService",
+]
