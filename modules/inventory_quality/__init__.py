eaefd2ea0bbaffd22c5c167dbd3db0ea25925ae6
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-09-01 14:11:07 -0400

    Register lineage-aware COA resolution

--- modules/inventory_quality/__init__.py
+++ modules/inventory_quality/__init__.py
@@ -3,6 +3,9 @@
 from .coa import CoaDocumentService, MAX_COA_BYTES, parse_coa_pdf
 from .models import CoaAnalyteResult, CoaDocument, LotQualityEvidence
 from .service import LotQualityService
+from .lineage_resolution import register_lineage_coa_resolution
+
+register_lineage_coa_resolution()
 
 __all__ = [
     "CoaAnalyteResult",
