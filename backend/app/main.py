19914df130f18c10afee4e39b5ef67e34434f4ad
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-09-04 11:00:22 -0400

    Register governed Metrc package router

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,6 +20,7 @@ from .routers.metrc_guide_v11 import router as metrc_guide_v11_router
 from .routers.metrc_readiness import router as metrc_readiness_router
 from .routers.metrc_cultivation_actions import router as metrc_cultivation_actions_router
 from .routers.metrc_harvest_actions import router as metrc_harvest_actions_router
+from .routers.metrc_package_actions import router as metrc_package_actions_router
 from .routers.audits import router as audit_router
 from .routers.plants import router as plants_router
 from .routers.cultivation_bulk import router as cultivation_bulk_router
@@ -249,6 +250,7 @@ app.include_router(metrc_guide_v11_router, prefix=settings.api_prefix)
 app.include_router(metrc_readiness_router, prefix=settings.api_prefix)
 app.include_router(metrc_cultivation_actions_router, prefix=settings.api_prefix)
 app.include_router(metrc_harvest_actions_router, prefix=settings.api_prefix)
+app.include_router(metrc_package_actions_router, prefix=settings.api_prefix)
 app.include_router(audit_router, prefix=settings.api_prefix)
 app.include_router(plants_router, prefix=settings.api_prefix)
 app.include_router(cultivation_bulk_router, prefix=settings.api_prefix)
