187dfc599fdc0be097f102a6602b8ca85f3bb217
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-09-04 11:04:41 -0400

    Add alpha operating mode module

---
+++ modules/alpha_mode/__init__.py
@@ -0,0 +1,13 @@
+from .models import AlphaOperatingMode
+from .service import (
+    ALPHA_OPERATING_MODES,
+    AlphaOperatingModeService,
+    AlphaOperatingModeState,
+)
+
+__all__ = [
+    "ALPHA_OPERATING_MODES",
+    "AlphaOperatingMode",
+    "AlphaOperatingModeService",
+    "AlphaOperatingModeState",
+]
