42640762d2d1b068485be04aa1dfa758d0d46c0a
MAVet710 <149276571+MAVet710@users.noreply.github.com>
2026-09-06 18:34:50 -0400

    Expose provider-neutral traceability contracts

--- modules/traceability/__init__.py
+++ modules/traceability/__init__.py
@@ -7,16 +7,28 @@ from .models import (
     TraceabilityTransactionAttempt,
 )
 from .processor import TraceabilityCredentials, process_queued, process_transaction
+from .provider_contract import (
+    ProviderExecutionResult,
+    TraceabilityCapability,
+    TraceabilityProviderAdapter,
+    TraceabilityProviderRegistry,
+    normalize_provider_result,
+)
 from .repository import TraceabilityRepository, VALID_TRANSITIONS
 
 __all__ = [
+    "ProviderExecutionResult",
     "TraceabilityBackofficeRepository",
+    "TraceabilityCapability",
     "TraceabilityCredentials",
+    "TraceabilityProviderAdapter",
+    "TraceabilityProviderRegistry",
     "TraceabilityRepository",
     "TraceabilityStatusEvent",
     "TraceabilityTransaction",
     "TraceabilityTransactionAttempt",
     "VALID_TRANSITIONS",
+    "normalize_provider_result",
     "process_queued",
     "process_transaction",
 ]
